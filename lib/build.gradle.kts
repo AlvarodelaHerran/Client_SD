93bcd5439714cdd08540220d18c2a78b71d26732
samaantha6 <samantha.munoz@opendeusto.es>
2026-01-15 18:52:17 +0100

    correction

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,12 +8,12 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation 'com.fasterxml.jackson.core:jackson-databind:2.15.2'
-    implementation 'com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2'
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
 }
 
 application {
-    mainClass = "Main"
+    mainClass.set("Main")
 }
 
 java {
